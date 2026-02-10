43d2367369c24830c1d2c3537ed89e36e60c8bc3
Shawn Ashleigh Yllaris M. Cruz <yllaris1004@gmail.com>
2026-02-10 18:02:30 +0800

    modified:   astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
 
-  // Replace with your details
-  site: 'https://YllariSang.github.io',
-  base: '/My Cool Portfolio',
+  site: 'https://yllarisang.github.io',
+  base: '/',
 });
